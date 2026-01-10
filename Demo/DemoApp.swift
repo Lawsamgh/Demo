d8a885593800206a5a1a7e4175ef431f13f92f58
Lawsamgh <ampenelawrence5@gmail.com>
2026-01-10 09:18:45 +0000

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by PGH_PICT_LAMPENE on 10/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
